a5328fb4b0886b06904802f3dbda53c4fc2d6009
Максим Сенокосенко <maxims@MBP-Maksim.lan>
2024-07-01 11:19:50 +0300

    Начало

---
+++ src/main/java/spring_config/LibraryProjectApplication.java
@@ -0,0 +1,11 @@
+package spring_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+}
